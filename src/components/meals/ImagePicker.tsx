7c6345e2649726af9b7cb8dd3622fa5b4b4efc97
tien998 <tienvn998@gmail.com>
2025-08-06 12:03:26 +0700

    add fix ImagePicker

--- src/components/meals/ImagePicker.tsx
+++ src/components/meals/ImagePicker.tsx
@@ -1,8 +1,8 @@
 'use client'
 
-import Image from 'next/image'
-import styles from './ImagePicker.module.css'
 import { useRef, useState, type ChangeEvent } from 'react'
+import styles from './ImagePicker.module.css'
+import Image from 'next/image'
 
 type Props = {
     label: string
@@ -11,16 +11,37 @@ type Props = {
 
 export default function ImagePicker({ label, name }: Props) {
 
+    const imageInput = useRef<HTMLInputElement>(null)
+    const [image, setImage] = useState<string | null>(null)
 
+    const handlePickImage = () => imageInput.current!.click()
+
+    const reader = new FileReader()
+    const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files![0]
+        if (!file)
+            return
+        reader.onload = (e) => setImage(String(e.target?.result))
+        reader.readAsDataURL(file)
+    }
     return (
         <div className={styles['picker']}>
             <label htmlFor={name}>{label}</label>
             <div className={styles['controls']}>
                 <div className={styles['preview']}>
-
+                    {image && <Image src={image} alt='Picked image' fill />}
                 </div>
-                <input className={styles['input']} />
-                <button type="button" className={styles['button']}>
+                <input
+                    className={styles['input']}
+                    type='file'
+                    accept='image/png, image/jpeg'
+                    id={name}
+                    name={name}
+                    ref={imageInput}
+                    onChange={handleChangeImage}
+                    required
+                />
+                <button type="button" className={styles['button']} onClick={handlePickImage}>
                     Pick an Image
                 </button>
             </div>
