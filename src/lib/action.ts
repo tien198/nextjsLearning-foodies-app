71902848f232b253cb7ffe7a2ee3f23bb7ebdec3
tien998 <tienvn998@gmai.com>
2025-08-07 16:24:40 +0700

    add validate input on server side

--- src/lib/action.ts
+++ src/lib/action.ts
@@ -4,6 +4,10 @@ import type Meal from "@/types/Meal"
 import { saveMeal } from "./mealsDbb"
 import { redirect } from "next/navigation"
 
+function isInvalidText(text?: string) {
+    return !text || text.trim() === ''
+}
+
 export async function shareMeal(formData: FormData) {
 
     const meal: Partial<Meal> = {
@@ -14,6 +18,18 @@ export async function shareMeal(formData: FormData) {
         creator: formData.get('name')?.toString() || '',
         creator_email: formData.get('email')?.toString() || '',
     }
+
+    if (
+        isInvalidText(meal.title)
+        || isInvalidText(meal.summary)
+        || isInvalidText(meal.instructions)
+        || isInvalidText(meal.creator)
+        || isInvalidText(meal.creator_email)
+        || !meal.image
+        || (meal.image as File).size === 0
+    )
+        throw new Error('InvalidInput')
+
     await saveMeal(meal as Meal)
     redirect('/meals')
 }
